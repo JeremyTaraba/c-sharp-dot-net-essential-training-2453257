5a509dddbfb6e76876eb78249522f22f6e3837b7
Jeremy Taraba <jeremytaraba@gmail.com>
2025-02-12 18:17:24 -0800

    Practicing c#

--- Start/Strings/Formatting/Program.cs
+++ Start/Strings/Formatting/Program.cs
@@ -3,9 +3,12 @@
 
 float f1 = 123.4f;
 int i1 = 2000;
+string f1String = "float";
+string i1String = "int";
 
 // TODO: Spacing and Alignment: Indexes
 
 
 // TODO: Spacing and Alignment: Interpolation
-
+Console.WriteLine($"{f1String, 0} {i1String, 10}");
+Console.WriteLine($"{f1, 0} {i1, 10}");
---
+++ Start/Test/TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int CalcHowManyDays(string date_str) {
+    int res = int.MaxValue;
+    DateTime result;
+    if (DateTime.TryParse(date_str, out result)) {
+        DateTime today = DateTime.Now;
+        TimeSpan span = today - result.AddMonths(3);
+        Console.WriteLine(span.ToString());
+        res = span.Days;
+        Console.WriteLine($"The date {date_str} is {res} days away.");
+    }
+
+    return res;
+}
+
+CalcHowManyDays("1/1 2025");
