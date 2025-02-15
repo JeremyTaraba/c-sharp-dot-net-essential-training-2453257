2d6e30b0fdbc13dddfef174363e31ed2611c9d32
Jeremy Taraba <jeremytaraba@gmail.com>
2025-02-14 17:52:34 -0800

    Update Program.cs

--- Start/Test/TestApp/Program.cs
+++ Start/Test/TestApp/Program.cs
@@ -1,19 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+Console.WriteLine("Hello");
 
-int CalcHowManyDays(string date_str) {
-    int res = int.MaxValue;
-    DateTime result;
-    if (DateTime.TryParse(date_str, out result)) {
-        DateTime today = DateTime.Now;
-        TimeSpan span = today - result.AddMonths(3);
-        Console.WriteLine(span.ToString());
-        res = span.Days;
-        Console.WriteLine($"The date {date_str} is {res} days away.");
-    }
+// create int array of size 10
 
-    return res;
-}
-
-CalcHowManyDays("1/1 2025");
+int[] numbers = new int[10];
